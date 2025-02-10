9881978cc23d46c89a90c864dfa41943ff69e46d
Tim Schroeder <tschroeder@zendesk.com>
2025-02-10 11:28:15 -0800

    v2.0.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Backslasher'
 maintainer_email 'nitz.raz@gmail.com'
 license          'GPL-2.0'
 description      'Installs/Configures python and python accessories'
-version          '1.1.0'
+version          '2.0.0'
 
 chef_version '>= 18.0.0'
 
